37fd2142a925566cd85696b63c8bc008f41ee171
Emilia <emilia.mena26@gmail.com>
2019-11-28 11:06:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return thankYouCards;
+}
+
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+countDown();
